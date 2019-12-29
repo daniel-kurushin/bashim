6a011c04f5bb7319ce32b3324e9f982b0f78e993
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2019-12-29 21:40:59 +0500

    new trigrams

--- trigrams.py
+++ trigrams.py
@@ -1,8 +1,14 @@
-from json import dump, load
+from json import dump, load, dumps
 from rutermextract import TermExtractor
+from pymorphy2 import MorphAnalyzer 
+from nltk import WordPunctTokenizer
+
 from pprint import pprint
 
+wpt = WordPunctTokenizer()
 te = TermExtractor()
+ma = MorphAnalyzer()
+
 data = load(open('./data/bashim.100.json', 'r'))
 
 #dump(data, open('/tmp/out.json', 'w'), indent=4, ensure_ascii=0)
@@ -23,20 +29,25 @@ for quo in data:
 
 messages = messages[:-1]
 
-pprint(messages)
+# pprint(messages)
 
 ngrams = []
 for message in messages:
     if message == "<|BEGIN|>":
         ngram = []
     elif message == "<|END|>":
-        ngrams += [ngram]
+        phrases = []
+        for phrase in ngram:
+            terms = list(set(te(phrase, strings=1, nested=1)))
+            words = list(set([ ma.parse(w)[0].normal_form for w in wpt.tokenize(phrase) ]))
+            phrases += [(terms, words, phrase)]
+        ngrams += [phrases]
     else:
         ngram += [message]
     
 #ngrams = set([ tuple(messages[i::i+3]) for i in range(len(messages) - 2)])
 
-pprint(ngrams)
+print(dumps(ngrams, ensure_ascii = 0, indent = 2))
 
 #rez = []
 #
