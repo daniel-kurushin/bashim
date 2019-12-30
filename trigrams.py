01ed7f2edf36fe915d1df6fbe5f4da701c08046b
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2019-12-30 20:04:10 +0500

    trigrams.py

--- trigrams.py
+++ trigrams.py
@@ -2,6 +2,7 @@ from json import dump, load, dumps
 from rutermextract import TermExtractor
 from pymorphy2 import MorphAnalyzer 
 from nltk import WordPunctTokenizer
+from time import time
 
 from pprint import pprint
 
@@ -9,12 +10,11 @@ wpt = WordPunctTokenizer()
 te = TermExtractor()
 ma = MorphAnalyzer()
 
-data = load(open('./data/bashim.100.json', 'r'))
+data = load(open('./data/bashim.json', 'r'))
 
 #dump(data, open('/tmp/out.json', 'w'), indent=4, ensure_ascii=0)
 
 messages = ["<|BEGIN|>"]
-n = 0
 for quo in data:
     try:
         for message in range(100):
@@ -32,31 +32,31 @@ messages = messages[:-1]
 # pprint(messages)
 
 ngrams = []
+n, m = 0, 0
+t = int(time())
+l = len(messages)
+
 for message in messages:
     if message == "<|BEGIN|>":
         ngram = []
     elif message == "<|END|>":
         phrases = []
         for phrase in ngram:
-            terms = list(set(te(phrase, strings=1, nested=1)))
+            terms = set(te(phrase, strings=1, nested=1))
             words = list(set([ ma.parse(w)[0].normal_form for w in wpt.tokenize(phrase) ]))
-            phrases += [(terms, words, phrase)]
+            idx = []
+            for word in words:
+                w = 1 if word in terms else .5
+                idx += [(w, word)]
+            phrases += [(idx, phrase)]
         ngrams += [phrases]
     else:
         ngram += [message]
+    n += 1
+    if time() - t > 1:
+        print("%s of %s, %s / sec" % (m, l, n))
+        m += n
+        n = 0
+        t = int(time())
     
-#ngrams = set([ tuple(messages[i::i+3]) for i in range(len(messages) - 2)])
-
-print(dumps(ngrams, ensure_ascii = 0, indent = 2))
-
-#rez = []
-#
-#for ngram in ngrams:
-#    keys = []
-#    for message in ngram:
-#        
-#        keys += [(message, terms)]
-#    rez += [keys]
-#    
-#
-#dump(rez, open('./data/trigrams.json', 'w'), indent=4, ensure_ascii=0)
+dump(ngrams, open('./data/trigrams.json', 'w'), indent=2, ensure_ascii=0)
